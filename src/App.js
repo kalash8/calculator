73d2f413980ec042f218f1a5dcb1cb7c79b03d52
kalash8 <kalashtelkar@gmail.com>
2024-03-02 05:58:42 +0530

    css designing done

--- src/App.js
+++ src/App.js
@@ -148,35 +148,37 @@ function App() {
   const [{ currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {});
 
   return (
-    <div className="calculator-grid">
+    <div className="container">
       <div className="output">
         <div className="previous-operand">{formatOperand(previousOperand) } {operation}</div>
         <div className="current-operand">{formatOperand(currentOperand) }</div>
       </div>
 
-      <button className="span-two" onClick={() => dispatch({type: ACTIONS.CLEAR})}>AC</button>
-      <button onClick={() => dispatch({type: ACTIONS.DELETE_DIGIT})}>DEL</button>
+    <div className="calculator-grid">
+      
+      
+
+      <div className="span-two" onClick={() => dispatch({type: ACTIONS.CLEAR})}><b>AC</b></div>
+      <button onClick={() => dispatch({type: ACTIONS.DELETE_DIGIT})}><b>DEL</b></button>
       <OperationsButton1 operation="/" dispatch={dispatch}/>
-      <DigitButton digit="1" dispatch={dispatch}/>
-      <DigitButton digit="2" dispatch={dispatch}/>
-      <DigitButton digit="3" dispatch={dispatch}/>
+      <DigitButton digit="7" dispatch={dispatch}/>
+      <DigitButton digit="8" dispatch={dispatch}/>
+      <DigitButton digit="9" dispatch={dispatch}/>
       <OperationsButton1 operation="*" dispatch={dispatch}/>
       <DigitButton digit="4" dispatch={dispatch}/>
       <DigitButton digit="5" dispatch={dispatch}/>
       <DigitButton digit="6" dispatch={dispatch}/>
       <OperationsButton1 operation="+" dispatch={dispatch}/>
-      <DigitButton digit="7" dispatch={dispatch}/>
-      <DigitButton digit="8" dispatch={dispatch}/>
-      <DigitButton digit="9" dispatch={dispatch}/>
+      <DigitButton digit="1" dispatch={dispatch}/>
+      <DigitButton digit="2" dispatch={dispatch}/>
+      <DigitButton digit="3" dispatch={dispatch}/>
       <OperationsButton1 operation="-" dispatch={dispatch}/>
-      <DigitButton digit="." dispatch={dispatch}/>
       <DigitButton digit="0" dispatch={dispatch}/>
-      <button className="span-two" onClick={() => dispatch({type: ACTIONS.EVALUATE})}> = </button>
-
+      <DigitButton digit="." dispatch={dispatch}/>
+      <div className="span-one" onClick={() => dispatch({type: ACTIONS.EVALUATE})}> = </div>
+    
       
-      <div className="footer">
-        <span>Kalash Telkar</span>
-      </div>
+    </div>
     </div>
     
   );
